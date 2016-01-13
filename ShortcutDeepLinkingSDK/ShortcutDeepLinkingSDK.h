8b59ab48c51d64b4f91ce9fd4af9c6c1f6aa3738
Severin Schoepke <severin.schoepke@gmail.com>
2016-01-13 11:30:55 +0100

    New release

--- ShortcutDeepLinkingSDK/ShortcutDeepLinkingSDK.h
+++ ShortcutDeepLinkingSDK/ShortcutDeepLinkingSDK.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Shortcut Media AG. All rights reserved.
 //
 
-#define SDK_VERSION @"0.0.1-beta4"
+#define SDK_VERSION @"0.0.1-beta5"
 
 #import "SCDeepLinking.h"
 #import "SCSession.h"
